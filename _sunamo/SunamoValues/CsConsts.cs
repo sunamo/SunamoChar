1ed7aab3cf62587895e35403e87aff6e203d9580
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-21 20:36:27 +0200

    Everyone on nuget

---
+++ _sunamo/SunamoValues/CsConsts.cs
@@ -0,0 +1,12 @@
+namespace SunamoChar;
+
+
+public class CsConsts
+{
+    #region For easy copy
+    public const string esteemed = "V�en�/�";
+    public const string dobryDen = "Dobr� den";
+    public const string Zdravim = "Zdrav�m";
+    public const string atSystemDot = "at System.";
+    #endregion
+}
