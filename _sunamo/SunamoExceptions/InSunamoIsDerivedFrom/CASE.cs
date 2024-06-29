82c7fdc4154352b72f051169d9dee7a368313277
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 08:57:37 +0200

    moved all in _sunamo to right subfolders by original project

---
+++ _sunamo/SunamoExceptions/InSunamoIsDerivedFrom/CASE.cs
@@ -0,0 +1,12 @@
+namespace SunamoChar;
+internal class CASE
+{
+    internal static bool IsListStringWrappedInArray<T>(List<T> v2)
+    {
+        var first = v2.First().ToString();
+        if (v2.Count == 1 && (first == "System.Collections.Generic.List`1[System.String]" ||
+        first == "System.Collections.Generic.List`1[System.Object]")) return true;
+
+        return false;
+    }
+}
