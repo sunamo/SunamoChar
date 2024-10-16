cf412fd67ac5335f00c615b9cdd2ac1221ae1e60
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-16 12:28:22 +0200

    Removed unused file

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -7,13 +7,6 @@ global using System.Text;
 global using System.Collections;
 global using System.IO;
 global using SunamoChar.Enums;
-global using SunamoChar;
 global using SunamoChar._sunamo.SunamoExceptions;
 global using System.Diagnostics;
-global using System.Diagnostics.CodeAnalysis;
 global using System.Net;
-global using SunamoChar;
-global using SunamoChar._sunamo;
-global using SunamoChar._sunamo.SunamoStringGetLines;
-
-global using SunamoChar._sunamo.SunamoValues;
--- Services/SpecialCharsService.cs
+++ Services/SpecialCharsService.cs
@@ -1,9 +1,5 @@
 namespace SunamoChar.Services;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 internal class SpecialCharsService
 {
