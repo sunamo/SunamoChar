0af0f4d160a7db42893bed33f1c23369d3a23ec4
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-28 09:44:29 +0100

    Buildable, removed unused internal methods in _sunamo

--- _sunamo/SH.cs
+++ _sunamo/SH.cs
@@ -2,34 +2,7 @@ namespace SunamoChar._sunamo;
 
 internal class SH
 {
-    internal static string TrimEnd(string name, string ext)
-    {
-        while (name.EndsWith(ext)) return name.Substring(0, name.Length - ext.Length);
 
-        return name;
-    }
-    internal static List<string> SplitCharMore(string v1, params char[] v2)
-    {
-        return v1.Split(v2).ToList();
-    }
 
-    internal static List<string> SplitMore(string s, params string[] dot)
-    {
-        return s.Split(dot, StringSplitOptions.RemoveEmptyEntries).ToList();
-    }
-
-    internal static string NullToStringOrDefault(object n)
-    {
-        //return NullToStringOrDefault(n, null);
-        return n == null ? " " + "(null)" : " " + n;
-    }
-    internal static string JoinNL(List<string> l)
-    {
-        StringBuilder sb = new();
-        foreach (var item in l) sb.AppendLine(item);
-        var r = string.Empty;
-        r = sb.ToString();
-        return r;
-    }
 
 }
